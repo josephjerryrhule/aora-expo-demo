635ffd06dcaff764f3c9af652d531b8f79f4f3e0
josephjerryrhule <josephjerryrhule@gmail.com>
2025-02-15 16:11:06 +0000

    Initial commit

---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Boomark = () => {
+    return (
+        <View>
+            <Text>Boomark</Text>
+        </View>
+    )
+}
+
+export default Boomark
